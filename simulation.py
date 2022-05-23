6396f3f818c39a02f4c41d3898f1067d53473921
maple <hongfengyeaaa@163.com>
2022-05-23 13:05:47 +0800

    1

--- simulation.py
+++ simulation.py
@@ -11,7 +11,7 @@ class Solver():
         self.max_iter = max_iter
         self.tol = tol
 
-    def solve_equlibrium(self, AT: torch.Tensor, BT: torch.Tensor, K:torch.Tensor):
+    def solve(self, AT: torch.Tensor, BT: torch.Tensor, K:torch.Tensor):
         AF = torch.zeros((self.batch_size, K.shape[0], 1)).to(self.device)
         BF = torch.zeros((self.batch_size, K.shape[1], 1)).to(self.device)
 
@@ -22,17 +22,28 @@ class Solver():
                 BF_last = BF
                 AF = torch.unsqueeze(AT, dim=2) / (K.matmul(BF)+1)
                 BF = torch.unsqueeze(BT, dim=1) / (K.T.matmul(AF)+1)
-                
                 err = (AF-AF_last).abs().max() + (BF-BF_last).abs().max()
                 if (err < self.tol):
                     break
-
             if (err > self.tol):
                 print(f'not converge in {iter} iterations')
 
-
+        return AF, BF
 
 
 if __name__ == '__main__':
-    solver = Solver(device=torch.device("cpu"), batch_size=1, mat_iter=10, tol=1e-3)
-    AT = 
+    solver = Solver(device=torch.device("cpu"), batch_size=1, max_iter=10, tol=1e-3)
+
+    AT = torch.Tensor([[1]])
+    BT = torch.Tensor([[1]])
+    K  = torch.Tensor([[1]])
+    AF, BF = solver.solve(AT, BT, K)
+    print(AF, BF)
+
+
+    AT = torch.Tensor([[1, 1]])
+    BT = torch.Tensor([[1, 1]])
+    K  = torch.Tensor([[1, 1], [1, 1]])
+    AF, BF = solver.solve(AT, BT, K)
+    print(AF, BF)
+    
