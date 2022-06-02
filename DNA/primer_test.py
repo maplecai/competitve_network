955460c0510f3372b595c769637f45e47bc67f0c
maple <hongfengyeaaa@163.com>
2022-06-02 13:29:54 +0800

    1

--- DNA/primer_test.py
+++ DNA/primer_test.py
@@ -1,6 +1,6 @@
 import primer3
 import numpy as np
-from utils import *
+from DNAutils import *
 
 class PrimerGenerator:
     def __init__(self):
@@ -16,10 +16,27 @@ class PrimerGenerator:
 
 if __name__ == '__main__':
     generator = PrimerGenerator()
-    for i in range(10):
+    dg_list = []
+    for i in range(3):
         s1 = generator.generate(20)
+        # a1 = primer3.calcTm(s1, tm_method='santalucia')
+        # print(a1)
         s2 = reverse_complement(s1)
-        a1 = primer3.calcTm(s1)
-        a2 = primer3.calcHeterodimer(s1, s2)
-        print(a1)
+        s2 = generator.generate(20)
+        a2 = primer3.calcHeterodimer(s1, s2, mv_conc=50, dv_conc=0, dntp_conc=0, temp_c=25, dna_conc=1)
+        
         print(a2)
+
+
+        t1 = a2.dh / a2.ds
+        print('在K=1时 T=', t1-273)
+
+        g = a2.dh - a2.ds*(25+273)
+        K = np.exp(-g / (1.987*(25+273)))
+        print('在T=25时 K=', K)
+        
+        g = a2.dh - a2.ds*(a2.tm+273)
+        K = np.exp(-g / (1.987*(a2.tm+273)))
+        print('在T=Tm时 K=', K)
+        # dna_conc指总单链浓度，GHS单位都是cal
+
