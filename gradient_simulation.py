cae59f68df301d626e797b9c828970fb1c00c357
maple <hongfengyeaaa@163.com>
2022-06-06 16:32:59 +0800

    完成计算偏导数的函数

--- gradient_simulation.py
+++ gradient_simulation.py
@@ -11,7 +11,7 @@ class Solver():
         self.max_iter = max_iter
         self.tol = tol
 
-    def solve(self, AT: torch.Tensor, BT: torch.Tensor, K:torch.Tensor):
+    def torch_solve(self, AT: torch.Tensor, BT: torch.Tensor, K:torch.Tensor):
         AF = torch.zeros((self.batch_size, K.shape[0], 1)).to(self.device)
         BF = torch.zeros((self.batch_size, K.shape[1], 1)).to(self.device)
 
@@ -31,30 +31,83 @@ class Solver():
             C = K * AF.matmul(BF.transpose(1, 2))
         return AF, BF, C
 
+    def np_solve_(self, AT: np.ndarray, BT: np.ndarray, K:np.ndarray):
+        # no batch
+        # K.shape = (nA, nB)
+        nA = len(AT)
+        nB = len(BT)
+        AT = AT.reshape(nA, 1)
+        BT = BT.reshape(1, nB)
+        AF, BF, C = self.np_solve(AT, BT, K)
+        AF = AF.reshape(nA)
+        BF = BF.reshape(nB)
+        return AF, BF, C
+
 
     def np_solve(self, AT: np.ndarray, BT: np.ndarray, K:np.ndarray):
-        # batch_size = 1
+        # no batch
         # AT.shape = (NA, 1)
         # BT.shape = (1, NB)
         AF = np.zeros(AT.shape)
         BF = np.zeros(BT.shape)
 
-        with torch.no_grad():
-            err = 0
-            for iter in range(self.max_iter):
-                AF_last = AF
-                BF_last = BF
-                AF = AT / ((K * BF).sum(axis=1, keepdims=True) + 1)
-                BF = BT / ((K * AF).sum(axis=0, keepdims=True) + 1)
-                # print('iter', iter, AF, BF)
-                err = np.abs(AF-AF_last).sum() + np.abs(BF-BF_last).sum()
-                if (err < self.tol):
-                    break
-            if (err > self.tol):
-                print(f'not converge in {iter} iterations')
+        err = 0
+        for iter in range(self.max_iter):
+            AF_last = AF
+            BF_last = BF
+            AF = AT / ((K * BF).sum(axis=1, keepdims=True) + 1)
+            BF = BT / ((K * AF).sum(axis=0, keepdims=True) + 1)
+            # print('iter', iter, AF, BF)
+            err = np.abs(AF-AF_last).sum() + np.abs(BF-BF_last).sum()
+            if (err < self.tol):
+                break
+        if (err > self.tol):
+            print(f'not converge in {iter} iterations')
 
-            C = K * AF * BF
+        C = K * AF * BF
         return AF, BF, C
+
+
+
+    def calc_partial_derivative(self, AF, BF, K, p=0, q=0):
+        # 先求解pA/pK, pB/pK, nA+nB维线性方程组
+        nA = len(AT)
+        nB = len(BT)
+        W = np.zeros((nA+nB, nA+nB))
+        b = np.zeros((nA+nB, 1))
+        # 一行一行填
+        for m in range(nA):
+            # 第m行
+            for j in range(nB):
+                # 第nA+j列
+                W[m, nA+j] += K[m, j] * AF[m]
+                # 第m列
+                W[m, m] += K[m, j] * BF[j]
+            W[m, m] += 1
+        for n in range(nB):
+            # 第nA+n行
+            for i in range(nA):
+                # 第i列
+                W[nA+n, i] += K[i, n] * BF[n]
+                # 第nA+n列
+                W[nA+n, nA+n] += K[i, n] * AF[i]
+            W[nA+n, nA+n] += 1
+        b[p, 0] += - AF[p] * BF[q]
+        b[nA+q, 0] += - AF[p] * BF[q]
+        
+        pApB = np.linalg.solve(W, b)
+        # print(pApB)
+
+        # 然后求pC/pK
+        pC = np.zeros((nA, nB))
+        for i in range(nA):
+            for j in range(nB):
+                pC[i, j] += K[i, j] * BF[n] * pApB[i] + K[i, j] * AF[m] * pApB[nA+j]
+        pC[p, q] += AF[p] * BF[q]
+
+        return pC
+
+
 
 
 # numpy version
@@ -63,10 +116,10 @@ if __name__ == '__main__':
     # 2v2
     
     solver = Solver(device=torch.device("cpu"), batch_size=1, max_iter=100, tol=1e-6)
-    AT = np.array([1., 1.]).reshape(2, 1)
-    BT = np.array([1., 1.]).reshape(1, 2)
+    AT = np.array([1., 1.])
+    BT = np.array([1., 1.])
     K  = np.array([1., 1., 1., 1.]).reshape(2, 2)
-    AF, BF, C = solver.np_solve(AT, BT, K)
+    AF, BF, C = solver.np_solve_(AT, BT, K)
     print('equilibrium state', AF, BF, C)
 
 
@@ -77,22 +130,27 @@ if __name__ == '__main__':
         K_ = K.copy()
         K_[0, 0] = K[0, 0]+ dK11
         print('K = ', K_)
-        AF_, BF_, C_ = solver.np_solve(AT_, BT_, K_)
+        AF_, BF_, C_ = solver.np_solve_(AT_, BT_, K_)
 
         # print(AF_, BF_, C_)
-        pC = (C_ - C) / dK11
-        print(f'numerical simulation partial C / partial K11 = ', pC)
+        pC1 = (C_ - C) / dK11
+        print('numerical simulation partial C / partial K11 = ', pC1)
 
 
     # theoretical
 
-    W = np.array([[1 + K[0,0]*AF[0,0] + K[0,0]*BF[0,0], K[0,0]*BF[0,0], K[0,0]*AF[0,0], 0],
-                  [K[0,1]*BF[0,1], 1 + K[0,1]*BF[0,1] + K[0,1]*AF[0,0], 0, K[0,1]*AF[0,0]],
-                  [K[1,0]*AF[1,0], 0, 1 + K[1,0]*AF[1,0] + K[1,0]*BF[0,0], K[1,0]*BF[0,0]],
-                  [0, K[1,1]*AF[1,0], K[1,1]*BF[0,1], 1 + K[1,1]*AF[1,0] + K[1,1]*BF[0,1]]])
-    b = np.array([[AF[0,0]*BF[0,0]], [0], [0], [0]])
-    ans = np.linalg.solve(W, b)
-    print('theoretocal partial C / partial K11 = ', ans.reshape(2,2))
+    W = np.array([[1 + K[0,0]*AF[0] + K[0,0]*BF[0], K[0,0]*BF[0], K[0,0]*AF[0], 0],
+                  [K[0,1]*BF[1], 1 + K[0,1]*BF[1] + K[0,1]*AF[0], 0, K[0,1]*AF[0]],
+                  [K[1,0]*AF[1], 0, 1 + K[1,0]*AF[1] + K[1,0]*BF[0], K[1,0]*BF[0]],
+                  [0, K[1,1]*AF[1], K[1,1]*BF[1], 1 + K[1,1]*AF[1] + K[1,1]*BF[1]]])
+    b = np.array([[AF[0]*BF[0]], [0], [0], [0]])
+    pC2 = np.linalg.solve(W, b)
+    pC2 = pC2.reshape(2,2)
+    print('theoretocal partial C / partial K11 = ', pC2)
+
+
+    pC3 = solver.calc_partial_derivative(AF, BF, K)
+    print('my function partial C / partial K11 = ', pC3)
 
 
 '''
