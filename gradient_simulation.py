497875549c2f18fd25aade387f5e3069a8e58680
maple <hongfengyeaaa@163.com>
2022-06-08 13:10:37 +0800

    06081310

--- gradient_simulation.py
+++ gradient_simulation.py
@@ -50,22 +50,19 @@ class Solver():
                 BF_last = BF
                 AF = AT / ((K * BF_last).sum(axis=1, keepdims=True) + 1)
                 BF = BT / ((K * AF_last).sum(axis=0, keepdims=True) + 1)
-                print('iter', iter, AF, BF)
+                # rint('iter', iter, AF, BF)
                 err = np.abs(AF-AF_last).sum() + np.abs(BF-BF_last).sum()
                 if (err < self.tol):
+                    # print(f'converge in {iter} iterations')
                     break
             if (err > self.tol):
                 print(f'not converge in {iter} iterations')
-
+            
             C = K * AF * BF
         return AF, BF, C
 
 
-    def torch_autogradient(self, AF, BF, K):
-        # alternative iteration
-        # AF = AT / ((K * BF).sum(axis=1, keepdims=True) + 1)
-        # BF = BT / ((K * AF).sum(axis=0, keepdims=True) + 1)
-
+    def torch_autograd(self, AF, BF, K):
         # simultaneous iteration
         AF_last = AF
         BF_last = BF
@@ -73,17 +70,34 @@ class Solver():
         BF = BT / ((K * AF_last).sum(axis=0, keepdims=True) + 1)
         C = K * AF * BF
 
-        pC0 = torch.zeros(K.shape)
+        pC = torch.zeros(K.shape)
         for i in range(K.shape[0]):
             for j in range(K.shape[1]):
                 C[i, j].backward(retain_graph=True)
-                pC0[i, j] = K.grad[0, 0]
+                pC[i, j] = K.grad[0, 0]
                 K.grad.detach_()
                 K.grad.zero_()
-                # print(pC0[i, j])
+                # print(pC[i, j])
 
-        return pC0
+        return pC
 
+
+    def torch_autograd2(self, AF, BF, K):
+        # alternative iteration
+        AF = AT / ((K * BF).sum(axis=1, keepdims=True) + 1)
+        BF = BT / ((K * AF).sum(axis=0, keepdims=True) + 1)
+        C = K * AF * BF
+
+        pC = torch.zeros(K.shape)
+        for i in range(K.shape[0]):
+            for j in range(K.shape[1]):
+                C[i, j].backward(retain_graph=True)
+                pC[i, j] = K.grad[0, 0]
+                K.grad.detach_()
+                K.grad.zero_()
+                # print(pC[i, j])
+
+        return pC
 
 
 
@@ -98,8 +112,6 @@ class Solver():
         AF = AF.reshape(nA)
         BF = BF.reshape(nB)
         return AF, BF, C
-
-
 
 
     def np_solve(self, AT: np.ndarray, BT: np.ndarray, K:np.ndarray):
@@ -129,8 +141,8 @@ class Solver():
 
     def calc_partial_derivative(self, AF, BF, K, p=0, q=0):
         # 先求解pA/pK, pB/pK, nA+nB维线性方程组
-        nA = len(AT)
-        nB = len(BT)
+        nA = len(AF)
+        nB = len(BF)
         W = np.zeros((nA+nB, nA+nB))
         b = np.zeros((nA+nB, 1))
         # 一行一行填
@@ -160,11 +172,22 @@ class Solver():
         pC = np.zeros((nA, nB))
         for i in range(nA):
             for j in range(nB):
-                pC[i, j] += K[i, j] * BF[n] * pApB[i] + K[i, j] * AF[m] * pApB[nA+j]
+                pC[i, j] += K[i, j] * BF[j] * pApB[i] + K[i, j] * AF[i] * pApB[nA+j]
         pC[p, q] += AF[p] * BF[q]
 
         return pC
 
+
+
+    def manual_2v2_partial_derivative(self, AF, BF, K):
+        W = np.array([[1 + K[0,0]*AF[0] + K[0,0]*BF[0], K[0,0]*BF[0], K[0,0]*AF[0], 0],
+                    [K[0,1]*BF[1], 1 + K[0,1]*BF[1] + K[0,1]*AF[0], 0, K[0,1]*AF[0]],
+                    [K[1,0]*AF[1], 0, 1 + K[1,0]*AF[1] + K[1,0]*BF[0], K[1,0]*BF[0]],
+                    [0, K[1,1]*AF[1], K[1,1]*BF[1], 1 + K[1,1]*AF[1] + K[1,1]*BF[1]]])
+        b = np.array([[AF[0]*BF[0]], [0], [0], [0]])
+        pC = np.linalg.solve(W, b)
+        pC = pC.reshape(2,2)
+        return pC
 
 
 
@@ -172,53 +195,45 @@ class Solver():
 if __name__ == '__main__':
 
     # 2v2 competition
-    solver = Solver(device=torch.device("cpu"), batch_size=1, max_iter=100, tol=1e-6)
+    solver = Solver(device=torch.device("cpu"), batch_size=1, max_iter=100, tol=1e-12)
     AT = np.array([1., 1.])
-    BT = np.array([1., 1.])
-    K  = np.array([1., 2., 3., 4.]).reshape(2, 2)
+    BT = np.array([1., 1., 1.])
+    K  = np.array([1., 2., 3., 4., 5., 6.]).reshape(2, 3)
     AF, BF, C = solver.np_solve_(AT, BT, K)
-    print('equilibrium state', AF, BF, C)
-
+    print('AT, BT, K')
+    print(AT, BT, K)
+    print('AF, BF, C')
+    print(AF, BF, C)
 
     # numerical simulation
-    for dK11 in [1e-1, 1e-2, 1e-3, 1e-4]:
+    for dK11 in [1e-4]:
         AT_ = AT
         BT_ = BT
         K_ = K.copy()
         K_[0, 0] = K[0, 0]+ dK11
-        print('K = ', K_)
+        # print('K = ', K_)
         AF_, BF_, C_ = solver.np_solve_(AT_, BT_, K_)
 
         # print(AF_, BF_, C_)
         pC1 = (C_ - C) / dK11
-        print('numerical simulation partial C / partial K00 = ', pC1)
+    print('numerical simulation pC/pK00')
+    print(pC1)
 
+    # manual
+    pC2 = solver.manual_2v2_partial_derivative(AF, BF, K)
+    print('my manual pC/pK00')
+    print(pC2)
 
     # theoretical
-    W = np.array([[1 + K[0,0]*AF[0] + K[0,0]*BF[0], K[0,0]*BF[0], K[0,0]*AF[0], 0],
-                  [K[0,1]*BF[1], 1 + K[0,1]*BF[1] + K[0,1]*AF[0], 0, K[0,1]*AF[0]],
-                  [K[1,0]*AF[1], 0, 1 + K[1,0]*AF[1] + K[1,0]*BF[0], K[1,0]*BF[0]],
-                  [0, K[1,1]*AF[1], K[1,1]*BF[1], 1 + K[1,1]*AF[1] + K[1,1]*BF[1]]])
-    b = np.array([[AF[0]*BF[0]], [0], [0], [0]])
-    pC2 = np.linalg.solve(W, b)
-    pC2 = pC2.reshape(2,2)
-    print('my handcraft partial C / partial K00 = ', pC2)
-
-    # theoretical 2
     pC3 = solver.calc_partial_derivative(AF, BF, K)
-    print('my function partial C / partial K00 = ', pC3)
+    print('my function pC/pK00')
+    print(pC3)
 
 
-
-
-    # torch version
-
-    solver = Solver(device=torch.device("cpu"), batch_size=1, max_iter=100, tol=1e-12)
-
-    # 1v1
+    # torch autograd
     AT = torch.Tensor([1, 1]).reshape(2,1)
-    BT = torch.Tensor([1, 1]).reshape(1,2)
-    K  = torch.Tensor([1, 2, 3, 4]).reshape(2,2)
+    BT = torch.Tensor([1, 1, 1]).reshape(1,3)
+    K  = torch.Tensor([1, 2, 3, 4, 5, 6]).reshape(2,3)
     K.requires_grad = True
 
     #print(K.grad)
@@ -229,6 +244,11 @@ if __name__ == '__main__':
     AF, BF, C = solver.torch_solve(AT, BT, K)
     # print(AF, BF, C)
     
-    pC0 = solver.torch_autogradient(AF, BF, K)
-    print('autograd partial C / partial K00', pC0)
+    pC4 = solver.torch_autograd(AF, BF, K)
+    print('torch autograd (simultaneous) pC/pK00')
+    print(pC4)
+
+    pC5 = solver.torch_autograd2(AF, BF, K)
+    print('torch autograd (alternative) pC/pK00')
+    print(pC5)
 
